cc6d2e9b7995c854c4fda851ef5e5da77d890ae5
AB-Siddiqe <siddiq-84dsdj@dipti.com.bd>
2024-12-18 22:36:40 +0600

    1st update

--- app/src/main/java/com/example/ab_mvvm/SplashActivity.java
+++ app/src/main/java/com/example/ab_mvvm/SplashActivity.java
@@ -1,8 +1,9 @@
 package com.example.ab_mvvm;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -15,11 +16,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
-    Animation top,bottom;
+    Animation top,bottom,left;
+
     TextView textView;
-    ImageView imageView;
+    ImageView imageView,imageView2;
     private long Splash;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,12 +35,32 @@ public class SplashActivity extends AppCompatActivity {
         });
         textView=findViewById(R.id.textView);
         imageView=findViewById(R.id.imageView);
+        imageView2=findViewById(R.id.imageView2);
+
 
         top= AnimationUtils.loadAnimation(this,R.anim.top_anim);
         bottom=AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+        left=AnimationUtils.loadAnimation(this,R.anim.left_anim);
 
         imageView.setAnimation(top);
         textView.setAnimation(bottom);
+        imageView2.setAnimation(left);
 
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this,CountDownActivity.class));
+                overridePendingTransition(R.anim.zoom_in,R.anim.static_animation);
+            }
+        });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                overridePendingTransition(R.anim.static_animation,R.anim.zoom_out);
+            }
+        });
     }
 }
